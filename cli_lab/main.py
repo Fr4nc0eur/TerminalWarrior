b3cda96fe136c4855a878148f4eddf5b70a6851b
Ban5hee <ban5hee@Windows.home>
2025-11-15 21:07:08 +0000

    Implemented docker function to work with level2_permissions

--- cli_lab/main.py
+++ cli_lab/main.py
@@ -1,14 +1,17 @@
 # cli_lab/main.py
-from cli_lab.levels import level1_intro
+from cli_lab.levels import level1_intro, level2_permissions
 
 def main():
     print("=== CLI Linux Lab ===")
     print("1) Level 1 - Intro Challenge")
+    print("2) Level 2 - Permissions")
     print("0) Exit")
     choice = input("Select a level: ").strip()
 
     if choice == "1":
         level1_intro.main()
+    elif choice == "2":
+        level2_permissions.main()
     else:
         print("Goodbye!")
 
